d5b16c12dd7336b70c653d037c7d9bbd00dd4cc5
Your name <Your email>
2023-06-28 03:18:49 +0200

    update get route to send formatted date to front

--- routes/trips.js
+++ routes/trips.js
@@ -1,17 +1,15 @@
 var express = require('express');
 const Trip = require('../models/trips');
 var router = express.Router();
-const moment = require('moment')
+const moment = require('moment');
 const { checkBody } = require('../modules/checkBody');
 
-
-
+//get all trips (mostly for tests)
 router.get("/all", (req, res) => {
     Trip.find().then((allTrips) => res.json({ result : true, allTrips }))
 })
 
-
-//FIXME : TIMEZONE IS FUCKED UP
+//get trips from departure, arrival & date
 router.post("/", (req, res) => {
     if (!checkBody(req.body, ["departure","arrival","date"])){
         res.json({result : false, error :  'Missing or empty fields' })
@@ -27,7 +25,7 @@ router.post("/", (req, res) => {
             if (!tripsData){
                 res.json({result : false, error : 'No trips available'})
             } else {
-                res.json({result : true, tripsData, time: tripsData.map(e=>moment(e.date).format('LT'))})
+                res.json({result : true, tripsData, time: tripsData.map(e=>moment(e.date).format('HH:mm'))})
             }
         })
     }
